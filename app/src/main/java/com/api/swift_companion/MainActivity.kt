dd1a96a4c6bf66607b83bcae3d1ed80b49c43883
LacrouxRaoni <lacroux.raoni@yahoo.com.br>
2024-12-11 18:47:56 -0300

    fist_screen

--- app/src/main/java/com/api/swift_companion/MainActivity.kt
+++ app/src/main/java/com/api/swift_companion/MainActivity.kt
@@ -13,35 +13,53 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.api.swift_companion.ui.theme.Swift_companionTheme
 
-class MainActivity : ComponentActivity() {
+
+import android.view.animation.AnimationUtils
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.animation.ObjectAnimator
+import android.animation.Animator
+import android.animation.ValueAnimator
+
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Swift_companionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContentView(R.layout.main_activity)
+
+        val editText = findViewById<EditText>(R.id.etBlinkingInput)
+
+        // Inicializa a cor padrão do hint
+        val defaultHintColor = editText.currentHintTextColor
+
+        // Animação de piscar para o hint
+        val blinkAnimation = ValueAnimator.ofFloat(1f, 0f).apply {
+            duration = 500
+            repeatMode = ValueAnimator.REVERSE
+            repeatCount = ValueAnimator.INFINITE
+            addUpdateListener { animator ->
+                val alpha = animator.animatedValue as Float
+                val hintColor = defaultHintColor and 0x00FFFFFF or ((alpha * 255).toInt() shl 24)
+                editText.setHintTextColor(hintColor)
+            }
+            start()
+        }
+
+        // Para a animação ao digitar
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty()) {
+                    blinkAnimation.cancel() // Para a animação
+                    editText.setHintTextColor(defaultHintColor) // Restaura a cor original
                 }
             }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Swift_companionTheme {
-        Greeting("Android")
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
